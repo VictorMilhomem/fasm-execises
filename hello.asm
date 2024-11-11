213178a9ee03a4908bf886b71ac070f12428b29d
VictorMilhomem <victormllosada@gmail.com>
2024-11-10 21:17:30 -0300

    fixing tabs

--- hello.asm
+++ hello.asm
@@ -5,16 +5,16 @@ segment readable executable
 entry main
 
 main:
-	lea rdi, [msg]	;; load the message into rdi register
-	mov rax, 14		;; put the length of the msg into rax register
-	mov	rdx, rax 	;; mov rax to rdx
-	mov rsi, rdi	;; mov rdi to rsi
-	mov rdi, 1		;; stdout
-	mov rax, 1		;; sys_write
+	lea		rdi, [msg]	;; load the message into rdi register
+	mov		rax, 14		;; put the length of the msg into rax register
+	mov		rdx, rax 	;; mov rax to rdx
+	mov		rsi, rdi	;; mov rdi to rsi
+	mov		rdi, 1		;; stdout
+	mov		rax, 1		;; sys_write
 	syscall
 
-	xor rdi, rdi	;; exit code 0
-	mov rax, 60		;; sys_exit
+	xor		rdi, rdi	;; exit code 0
+	mov		rax, 60		;; sys_exit
 	syscall
 
 
